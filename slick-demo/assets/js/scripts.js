bf0370ed98e598f613b80548a28d7b60235dc2d6
mj-www <maxlastname@protonmail.com>
2026-05-10 18:19:03 -0500

    Slick demo assignment submission

---
+++ slick-demo/assets/js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: true,
+      infinite: false,
+      speed: 500,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: false,
+    });
+
+
+});
